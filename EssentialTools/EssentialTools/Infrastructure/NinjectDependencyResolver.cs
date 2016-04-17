9e97d4e021b4f8c68ed121bfec0ac3a355c36367
zhaokuo <1014336691@qq.com>
2016-04-17 14:24:48 +0800

    创建依赖解析器

---
+++ EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using EssentialTools.Models;
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver()
+        {
+            kernel = new StandardKernel();//创建内核
+            AddBindings();//添加绑定, 
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType;
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculater>();
+        }
+    }
+}
