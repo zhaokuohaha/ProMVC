d7fcf9853cf9b32094be037b88a8f1144fc91bb2
zhaokuo <1014336691@qq.com>
2016-04-17 10:03:36 +0800

    第四章, 语言特性  --- 使用对象初始化器

---
+++ languageFeatures/languageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace languageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Index动作
+        public string Index()
+        {
+            return "Navigate wo a URL to show an example";
+        }
+    }
+}
