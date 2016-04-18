3aa86f7b290afe1e0e7e849d8726a9b76c508056
zhaokuo <1014336691@qq.com>
2016-04-18 16:36:10 +0800

    添加分页及其单元测试, 如果要在单元测试项目中测试控制器, 需要添加System.Web.Mvc的引用,但是一开始找不到,所以需要去NuGet程序包管理区下载

--- SportStore/SportStore.UnitTests/UnitTest1.cs
+++ SportStore/SportStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+using SportStore.WebUI.Controllers;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace SportStore.UnitTests
 {
@@ -7,8 +13,29 @@ namespace SportStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Pageinate()
         {
+            //准备
+            Mock<IProuctRepository> mock = new Mock<IProuctRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "p1" },
+                new Product {ProductID = 2, Name = "p2" },
+                new Product {ProductID = 3, Name = "p3" },
+                new Product {ProductID = 4, Name = "p4" },
+                new Product {ProductID = 5, Name = "p5" },
+            }.AsQueryable());
+            ProductController controller = new ProductController(mock.Object);
+            controller.pageSize = 3;
+
+            //动作
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            //断言
+            Product[] proArray = result.ToArray();
+            Assert.IsTrue(proArray.Length == 2);
+            Assert.AreEqual(proArray[0].Name, "p4");
+            Assert.AreEqual(proArray[2].Name, "p5");
         }
     }
 }
--- SportStore/SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using SportStore.Domain.Abstract;
-using SportStore.Domain.Entities;
 namespace SportStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProuctRepository repository;
+        public int pageSize = 4;
         public ProductController(IProuctRepository repository)
         {
             this.repository = repository;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products.OrderBy(p=>p.ProductID).Skip((page -1)*pageSize).Take(pageSize));
         }
     }
 }
