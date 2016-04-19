74a4aee1af811e83e889e3008c468e40a55eb6c0
zhaokuo <1014336691@qq.com>
2016-04-19 13:53:29 +0800

    购物车实现---添加到购物车,  继续购物

---
+++ SportStore/SportStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Entities;
+using SportStore.Domain.Abstract;
+using SportStore.WebUI.Models;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        /// <summary>
+        /// 商品集合
+        /// </summary>
+        private IProuctRepository repository;
+        
+        public CartController(IProuctRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        /// <summary>
+        /// 读取购物车, 首先从Session读取, 读不到则创建一个并写入Session
+        /// </summary>
+        /// <returns></returns>
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if(cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        /// <summary>
+        /// 添加到购物车
+        /// </summary>
+        /// <param name="productId">商品id</param>
+        /// <param name="returnUrl">重定向Url</param>
+        /// <returns></returns>
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if(product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if(product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+        });
+        }
+}
+}
---
+++ SportStore/SportStore.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportStore.Domain.Entities;
+namespace SportStore.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
