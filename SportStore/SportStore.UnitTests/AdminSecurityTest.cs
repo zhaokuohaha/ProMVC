30b12eae6613db7323c205fd1dfe9af2451ead4c
zhaokuo <1014336691@qq.com>
2016-04-21 12:40:25 +0800

    用户管理员权限设置---单元测试通过了,说明登录成功之后已经跳到Index页面去了, 但是实际运行的时候又回来了, Admin/Index那边是怎样判断用户是否正确登录? 不是使用存好的cookie嘛

---
+++ SportStore/SportStore.UnitTests/AdminSecurityTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportStore.WebUI.Controllers;
+using SportStore.WebUI.Infrastructure.Abstrat;
+using SportStore.WebUI.Models;
+using System.Web.Mvc;
+
+
+namespace SportStore.UnitTests
+{
+    [TestClass]
+    public  class AdminSecurityTest
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            //准备 --- 创建模仿认证提供器
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "admin")).Returns(true);
+            //准备 --- 创建视图模型
+            LoginViewModelcs model = new LoginViewModelcs
+            {
+                UserName = "admin",
+                Password = "admin"
+            };
+            //准备 --- 创建控制器
+            AccountController target = new AccountController(mock.Object);
+
+            //动作 --- 使用合法的凭证认证
+            ActionResult result = target.Login(model, "/MyURL");
+
+            //断言
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("/MyURL", ((RedirectResult)result).Url);
+        }
+
+
+        [TestMethod]
+        public void Cannot_Login_With_InValid_Credentials()
+        {
+            //准备 --- 创建模仿认证提供器
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("baduser", "badpassowrd")).Returns(false);
+            //准备 --- 创建视图模型
+            LoginViewModelcs model = new LoginViewModelcs
+            {
+                UserName = "baduser",
+                Password = "badpassword"
+            };
+            //准备 --- 创建控制器
+            AccountController target = new AccountController(mock.Object);
+
+            //动作 --- 使用合法的凭证认证
+            ActionResult result = target.Login(model, "/MyURL");
+
+            //断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
+    }
+}
---
+++ SportStore/SportStore.WebUI/Models/LoginViewModelcs.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportStore.WebUI.Models
+{
+    public class LoginViewModelcs
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
